376103081181d3dc625ff021ed6b04d9335685d3
patrickmbasco <59862023+patrickmbasco@users.noreply.github.com>
2020-09-06 17:00:25 -0700

    Add files via upload

---
+++ Python Fundamentals/forloop1.py
@@ -0,0 +1,26 @@
+# Basic - Print all the numbers/integers from 0 to 150.
+# for i in range(0,151):
+#     print(i)
+
+#Multiples of Five - Print all the multiples of 5 from 5 to 1,000,000.
+# for i in range(5,1000005,5):
+#     print(i)
+
+# Counting, the Dojo Way - Print integers 1 to 100.  If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
+# for i in range(0,101):
+#     if i % 5 == 0:
+#         print("Coding")
+#     elif i % 10 == 0 and i % 5 == 0:
+#         print("Dojo")
+#     else:
+#         print(i)
+
+# Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+# sum = 0
+# for i in range (1,500000,2):
+#     sum += i
+#     print(sum)
+
+# Countdown by Fours - Print positive numbers starting at 2018, counting down by fours (exclude 0).
+for i in range (2018, 4, -4):
+    print(i)
